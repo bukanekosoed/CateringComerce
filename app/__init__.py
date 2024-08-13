3e60e8252fe7d9329422cc866e040a71ef918f1f
bukanekosoed <ekosudarwanto98@gmail.com>
2024-08-13 07:41:21 +0700

    --new

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,10 @@
 from flask import Flask,render_template
 from config import Config
-from pymongo import MongoClient
 from flask_session import Session
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
-    # Setup MongoDB
-    client = MongoClient(app.config['MONGO_URI'])
-    app.db = client[app.config['DB_NAME']]
-
-    # Debugging: cek apakah koneksi berhasil
-    try:
-        app.db.command('ping')  # Ping database untuk memverifikasi koneksi
-        print("MongoDB connected successfully")
-    except Exception as e:
-        print(f"Error connecting to MongoDB: {e}")
 
     # Setup session
     Session(app)
